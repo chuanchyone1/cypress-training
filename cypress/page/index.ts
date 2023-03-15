616d5299176ba195a55d93e4de803de9781073bb
Juan Torres <juan.torresh@perficient.com>
2023-03-15 14:53:55 -0500

    pom

--- cypress/page/index.ts
+++ cypress/page/index.ts
@@ -4,4 +4,4 @@ export { ItemPages } from "./item.page";
 export { CartPage } from "./shopping-cart.page";
 export { InformationPage } from "./information.page";
 export { OverView } from "./overview.page";
-export { Finish } from "./checkout-complete.page";.
+export { Finish } from "./checkout-complete.page";
